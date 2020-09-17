47c38878ec7caa4006c1693d2fad2ec3d2b643b8
Mark Brown <mrb5142@rit.edu>
2020-09-17 13:49:54 -0500

    Updated resume blurbs, added WillPopScope

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -62,9 +62,11 @@ const RESOURCES = {
 "icons/icon-512.png": "1885d08306e1b0a725861f0d29a03753",
 "index.html": "be0a7d01b9b71dd3ee522b3487f698bf",
 "/": "be0a7d01b9b71dd3ee522b3487f698bf",
-"main.dart.js": "67a4f62623345f8062e5f06078f27f58",
+"main.dart.js": "42fbea33dbaf59a3a3814b4405c936f7",
 "manifest.json": "762226e3634c7a39b4cc48e27587d314",
-"myjs.js": "8c9be47ad7c6d132c48befeb15855418"
+"myjs.js": "8c9be47ad7c6d132c48befeb15855418",
+"OneSignalSDKUpdaterWorker.js": "ebb63ca15bba16b550232b0b0f66c726",
+"OneSignalSDKWorker.js": "ebb63ca15bba16b550232b0b0f66c726"
 };
 
 // The application shell files that are downloaded before a service worker can
